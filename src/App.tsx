e94b7cb40bfef44ad90197650dea6c928c0c4f04
Filip Sjölander <filip@sanalabs.com>
2024-03-29 17:42:22 +0100

    recognize lines in screens

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,8 @@ const BodyStyle = createGlobalStyle`
 
 type Point = [number, number]
 
+type Line = { startPoint: Point; endPoint: Point }
+
 type Size = [number, number]
 
 type Screen = {
@@ -47,6 +49,29 @@ const getScreenFromTwoPoints = ([x1, y1]: Point, [x2, y2]: Point): Screen => {
   }
 }
 
+const pointIsInsideScreen = (point: Point, screen: Screen): boolean => {
+  const { topLeft, bottomRight } = screen
+  const [xMin, yMin] = topLeft
+  const [xMax, yMax] = bottomRight
+  const [pointX, pointY] = point
+
+  return xMin <= pointX && pointX <= xMax && yMin <= pointY && pointY <= yMax
+}
+
+const getScreenAsLines = (screen: Screen): [Line, Line, Line, Line] => {
+  const { topLeft, topRight, bottomLeft, bottomRight } = screen
+  return [
+    { startPoint: topLeft, endPoint: topRight },
+    { startPoint: topRight, endPoint: bottomRight },
+    { startPoint: bottomRight, endPoint: bottomLeft },
+    { startPoint: bottomLeft, endPoint: topLeft },
+  ]
+}
+
+const lineIsInsideScreen = (line: Line, screen: Screen): boolean => {
+  return pointIsInsideScreen(line.startPoint, screen) && pointIsInsideScreen(line.endPoint, screen)
+}
+
 const mapPointToScreenSpace = ([x, y]: Point, [screenSizeX, screenSizeY]: Size): Point => {
   return [x * screenSizeX, y * screenSizeY]
 }
@@ -55,10 +80,31 @@ const mapPointFromScreenSpace = ([x, y]: Point, [screenSizeX, screenSizeY]: Size
   return [x / screenSizeX, y / screenSizeY]
 }
 
+const getLinesInsideScreen = (screen1: Screen, lines: Line[]): Line[] => {
+  return lines.filter(it => lineIsInsideScreen(it, screen1))
+}
+
+const drawLine = (ctx: CanvasRenderingContext2D, line: Line): void => {
+  const { startPoint, endPoint } = line
+
+  const screenSize: Size = [ctx.canvas.width, ctx.canvas.height]
+
+  ctx.lineWidth = 2
+  ctx.strokeStyle = 'white'
+
+  ctx.beginPath()
+  ctx.moveTo(...mapPointToScreenSpace(startPoint, screenSize))
+  ctx.lineTo(...mapPointToScreenSpace(endPoint, screenSize))
+  ctx.stroke()
+}
+
 const drawScreen = (ctx: CanvasRenderingContext2D, screen: Screen): void => {
   const { topLeft, topRight, bottomLeft, bottomRight } = screen
 
   const screenSize: Size = [ctx.canvas.width, ctx.canvas.height]
+
+  ctx.lineWidth = 2
+  ctx.strokeStyle = '#faf'
 
   ctx.beginPath()
   ctx.moveTo(...mapPointToScreenSpace(topLeft, screenSize))
@@ -67,8 +113,6 @@ const drawScreen = (ctx: CanvasRenderingContext2D, screen: Screen): void => {
   ctx.lineTo(...mapPointToScreenSpace(bottomLeft, screenSize))
   ctx.lineTo(...mapPointToScreenSpace(topLeft, screenSize))
 
-  ctx.lineWidth = 2
-  ctx.strokeStyle = '#faf'
   ctx.stroke()
 }
 
@@ -136,7 +180,23 @@ function App() {
         const p0 = draftScreenOrigin
         const p1 = mousePositionRef.current
 
-        drawScreen(ctx, getScreenFromTwoPoints(p0, p1))
+        const draftScreen = getScreenFromTwoPoints(p0, p1)
+
+        drawScreen(ctx, draftScreen)
+
+        const [screen1] = screens
+
+        if (screen1 !== undefined && draftScreen !== undefined) {
+          console.log({ screen1, draftScreen, screensLength: screens.length })
+          const linesOfScreen2 = getScreenAsLines(draftScreen)
+          const linesInScreen = getLinesInsideScreen(screen1, linesOfScreen2)
+          if (linesInScreen.length > 0) {
+            console.log({ linesInScreen })
+            for (const line of linesInScreen) {
+              drawLine(ctx, line)
+            }
+          }
+        }
       }
       requestAnimationFrame(drawFrame)
     }
@@ -164,10 +224,16 @@ function App() {
           const mousePoint = getMousePoint(e)
 
           // convert draft screen to real screen
-          setScreens(prev => [...prev, getScreenFromTwoPoints(draftScreenOrigin, mousePoint)])
+
+          const newScreen = getScreenFromTwoPoints(draftScreenOrigin, mousePoint)
+          const { topLeft, bottomRight } = newScreen
+          const [xMin, yMin] = topLeft
+          const [xMax, yMax] = bottomRight
+          setDraftScreenOrigin(undefined)
+          if (xMax - xMin < 0.01 || yMax - yMin < 0.01) return
+          setScreens(prev => [...prev, newScreen])
 
           // reset draft screen
-          setDraftScreenOrigin(undefined)
         }}
         onMouseMove={e => {
           mousePositionRef.current = getMousePoint(e)
